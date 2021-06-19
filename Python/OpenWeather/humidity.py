698bcea7ca769a48765efce8231dd5a68a713df4
Christopher Marshall C <50683885+Perseus392@users.noreply.github.com>
2021-06-19 16:48:49 +0530

    Update humidity.py

--- Python/OpenWeather/humidity.py
+++ Python/OpenWeather/humidity.py
@@ -3,7 +3,7 @@ import requests
 
 def main(args):
     city = 'Delhi'
-    apikey = '4f9243cae71d455b452f3e84f50be078'
+    apikey = args.key
     url = 'https://api.openweathermap.org/data/2.5/weather?q={0}&appid={1}'.format(city, apikey)
     res = requests.get(url)
     data = res.json()
